421be977460b9d45668446b2bc6bd42ecda76e19
Jordan <jclemens24@hotmail.com>
2022-07-16 13:29:27 -0400

    Edited: CharacterParity.ts to remove Version2 solution which did not cover all possibilities

--- C/CharacterParity.ts
+++ C/CharacterParity.ts
@@ -10,11 +10,3 @@ function CharacterParity(symbol: string): string {
 console.log(CharacterParity('5'));
 console.log(CharacterParity('8'));
 console.log(CharacterParity('q'));
-
-function CharacterParityV2(symbol: string): string {
-  return parseInt(symbol, 10) % 2 ? 'odd' : 'even';
-}
-
-console.log(CharacterParityV2('5'));
-console.log(CharacterParityV2('8'));
-console.log(CharacterParityV2('10'));
